b3eba453e54588d3995b1f2c72882d6832d3586f
UladzislauBaranski <vladbaransky13@gmail.com>
2021-01-25 15:39:35 +0300

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports =   function reverse (n) {
+    let z=Math.abs(n);
+    var b, sum = 0;
+    while(z != 0)
+    {
+      b = z % 10;
+      sum = sum * 10 + b;
+      z = Math.floor(z / 10);
+    }
+    return sum;
 }
